c972e89fae0ced7ffc12e11f1cc2819ca7063b0a
t0ffe <toffelaakso@gmail.com>
2024-11-04 20:55:23 +0200

    15

--- viikko1/nhl-statistics-1/src/index.py
+++ viikko1/nhl-statistics-1/src/index.py
@@ -1,8 +1,12 @@
+from player_reader import PlayerReader
 from statistics_service import StatisticsService
 
 
 def main():
-    stats = StatisticsService()
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
+    reader = PlayerReader(url)
+
+    stats = StatisticsService(reader)
     philadelphia_flyers_players = stats.team("PHI")
     top_scorers = stats.top(10)
 
--- viikko1/nhl-statistics-1/src/statistics_service.py
+++ viikko1/nhl-statistics-1/src/statistics_service.py
@@ -1,11 +1,8 @@
 from player_reader import PlayerReader
 
 
-
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
-
+    def __init__(self, reader: PlayerReader):
         self._players = reader.get_players()
 
     def search(self, name):
@@ -16,10 +13,7 @@ class StatisticsService:
         return None
 
     def team(self, team_name):
-        players_of_team = filter(
-            lambda player: player.team == team_name,
-            self._players
-        )
+        players_of_team = filter(lambda player: player.team == team_name, self._players)
 
         return list(players_of_team)
 
@@ -28,11 +22,7 @@ class StatisticsService:
         def sort_by_points(player):
             return player.points
 
-        sorted_players = sorted(
-            self._players,
-            reverse=True,
-            key=sort_by_points
-        )
+        sorted_players = sorted(self._players, reverse=True, key=sort_by_points)
 
         result = []
         i = 0
