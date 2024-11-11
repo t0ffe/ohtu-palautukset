cefb0e8d6adba422ca1250d32215f13e1a3ce128
t0ffe <toffelaakso@gmail.com>
2024-11-11 21:19:33 +0200

    v2t13

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,14 +1,46 @@
 from player import PlayerReader, PlayerStats
+from rich.console import Console
+from rich.table import Table
+from rich.prompt import Prompt
+
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    url = "https://studies.cs.helsinki.fi/nhlstats"
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
+    console = Console()
+    seasons = ["2018-19", "2019-20", "2020-21", "2021-22", "2022-23", "2023-24", "2024-25"]
 
-    print("Players from FIN\n")
-    for player in players:
-        print(player)
+    console.print("\nNHL Statistics by nationality", style="italic")
+    
+    season = Prompt.ask(f"\nSelect season [cyan][{ '/'.join(seasons)}][/]").strip()
+
+    while season not in seasons:
+        season = Prompt.ask(f"\nInvalid season {season}. Please select one of the following: [cyan][{ '/'.join(seasons)}][/]").strip()
+    
+    while True:
+        nationalities = reader.get_nationalities(season)
+        nationality = Prompt.ask(f"\nSelect nationality [cyan][{ '/'.join(nationalities)}][/]").strip().upper()
+        
+        while nationality not in nationalities:
+            nationality = Prompt.ask(f"\nInvalid nationality {nationality}. Please select one of the following: [cyan][{ '/'.join(nationalities)}][/]").strip().upper()
+            continue
+
+        players = stats.top_scorers_by_nationality(nationality, season)
+
+        table = Table(title=f"Top Scorers from {nationality} in {season}")
+
+        table.add_column("Name", justify="left", style="cyan", no_wrap=True)
+        table.add_column("Team", justify="left", style="magenta")
+        table.add_column("Goals", justify="right", style="green")
+        table.add_column("Assists", justify="right", style="green")
+        table.add_column("Total Points", justify="right", style="bold")
+
+        for player in players:
+            table.add_row(player.name, player.team, str(player.goals), str(player.assists), str(player.goals + player.assists))
+
+
+        console.print(table)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -14,17 +14,23 @@ class PlayerReader:
     def __init__(self, url):
         self.url = url
 
-    def get_players(self):
-        response = requests.get(self.url).json()
+    def get_players(self, season):
+        url = f"{self.url}/{season}/players"
+        response = requests.get(url).json()
         players = [Player(player_dict) for player_dict in response]
         return players
+    
+    def get_nationalities(self, season):
+        players = self.get_players(season)
+        nationalities = sorted(set([player.nationality for player in players]), key=lambda n: n.lower())
+        return nationalities
 
 class PlayerStats:
     def __init__(self, reader):
         self.reader = reader
 
-    def top_scorers_by_nationality(self, nationality):
-        players = self.reader.get_players()
+    def top_scorers_by_nationality(self, nationality, season):
+        players = self.reader.get_players(season)
         filtered_players = filter(lambda p: p.nationality == nationality, players)
         sorted_players = sorted(filtered_players, key=lambda p: p.goals + p.assists, reverse=True)
         return sorted_players
