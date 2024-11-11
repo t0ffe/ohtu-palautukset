3c7fce77b8f716e70a985d025cd704e5029f8b2d
t0ffe <toffelaakso@gmail.com>
2024-11-11 20:04:21 +0200

    v2t10

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,23 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    print("Players from FIN")
+
+    for player in players:
+        if player.nationality == "FIN":
+            print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,10 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.team = dict['team']
+        self.nationality = dict['nationality']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
     
     def __str__(self):
-        return self.name
+        return f"{self.name} team {self.team}  goals {self.goals} assists {self.assists}"
