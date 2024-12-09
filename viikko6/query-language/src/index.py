65a7d66998146ac5c834af679f9b3f7ae99f69d0
t0ffe <toffelaakso@gmail.com>
2024-12-09 20:17:17 +0200

    v6t5

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -10,10 +10,16 @@ def main():
     
     matcher = (
         query
-        .plays_in("NYR")
-        .has_at_least(10, "goals")
-        .has_fewer_than(20, "goals")
-        .build()
+            .one_of(
+                query.plays_in("PHI")
+                    .has_at_least(10, "assists")
+                    .has_fewer_than(10, "goals")
+                    .build(),
+                query.plays_in("EDM")
+                    .has_at_least(50, "points")
+                    .build()
+            )
+            .build()
     )
 
     for player in stats.matches(matcher):
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -1,3 +1,5 @@
+import copy
+
 class And:
     def __init__(self, *matchers):
         self._matchers = matchers
@@ -66,16 +68,24 @@ class QueryBuilder:
         self._matcher = All()
 
     def plays_in(self, team):
-        self._matcher = And(self._matcher, PlaysIn(team))
-        return self
+        new_builder = copy.deepcopy(self)
+        new_builder._matcher = And(new_builder._matcher, PlaysIn(team))
+        return new_builder
 
     def has_at_least(self, value, attr):
-        self._matcher = And(self._matcher, HasAtLeast(value, attr))
-        return self
+        new_builder = copy.deepcopy(self)
+        new_builder._matcher = And(new_builder._matcher, HasAtLeast(value, attr))
+        return new_builder
 
     def has_fewer_than(self, value, attr):
-        self._matcher = And(self._matcher, HasFewerThan(value, attr))
-        return self
+        new_builder = copy.deepcopy(self)
+        new_builder._matcher = And(new_builder._matcher, HasFewerThan(value, attr))
+        return new_builder
+
+    def one_of(self, *matchers):
+        new_builder = copy.deepcopy(self)
+        new_builder._matcher = Or(*matchers)
+        return new_builder
 
     def build(self):
         return self._matcher
